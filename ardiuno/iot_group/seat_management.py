623adbca82d1c9d15253ed6a38554c8fa675867c
ken-arakawa <130194666+ken-arakawa@users.noreply.github.com>
2024-01-20 22:15:49 +0900

    Update seat_management.py

--- ardiuno/iot_group/seat_management.py
+++ ardiuno/iot_group/seat_management.py
@@ -4,6 +4,8 @@ import json
 
 ser = serial.Serial('COM5', 9600)
 
+previous_is_used = {}
+
 while True:
     arduino_data = ser.readline().decode('utf-8').strip()
     
@@ -12,6 +14,16 @@ while True:
         chair_number = int(data[0].split(':')[1].strip())
         is_used = int(data[2].split(':')[1].strip())
         
-        json_data = {"chair": chair_number, "is_used": is_used}
+        if chair_number in previous_is_used and is_used != previous_is_used[chair_number]:
         
-        print("Received JSON data:", json_data)
+            url = f"http://127.0.0.1:5000/chair?charNo={chair_number}&is_used={is_used}"
+            
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                print("HTTP GET requests successful")
+                previous_is_used[chair_number] = is_used
+            except requests.exceptions.RequestException as e:
+                print(f"HTTP GET request failed: {e}")
+        else:
+            previous_is_used[chair_number] = is_used
